3d55bdb22bfa4087708e0c20292cbfc7816aa4f1
Emirhan Taşdeviren <emirhantasdeviren@gmail.com>
2024-07-06 17:23:29 +0300

    Add keymap for git fetch command

--- lua/marknikky/plugins/fugitive.lua
+++ lua/marknikky/plugins/fugitive.lua
@@ -4,6 +4,9 @@ local M = {
 
 M.config = function()
     vim.keymap.set('n', "<leader>gs", vim.cmd.Git, { desc = "Git Status" })
+    vim.keymap.set('n', "<leader>gf", function()
+        vim.cmd.Git({ args = { "fetch" } })
+    end, { desc = "Git Fetch" })
 end
 
 return M
